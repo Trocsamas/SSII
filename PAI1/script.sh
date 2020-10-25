a9a3270b17b2e942015097527c0ebf797eb7a0b7
Trocsamas <jesala31@gmail.com>
2020-10-25 21:36:28 +0100

    Script para la generacion de hashes
    Aún falta completar distintas opciones, entre ellas el tiempo entre ejecución, la comparación de ficheros y un par de pequeñas opciones para hacerlo más amistoso. #16

--- PAI1/script.sh
+++ PAI1/script.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+encryption=256
+function usage()
+{
+    cat<<USAGE
+Este es un script que maneja de forma automática un hids
+usage: ${0##*/} [options] [path]
+
+  Options:
+   -h, --help           display this help
+   -V, --version        display version information
+   --md5                Use of MD5 as encryption method
+   --sha1               Use of SHA1 as encryption method
+   --sha224             Use of SHA224 as encryption method
+   --sha256(default)    Use of SHA256 as encryption method
+   --sha384             Use of SHA384 as encryption method
+   --sha512             Use of SHA512 as encryption method
+
+USAGE
+}
+
+function version()
+{
+    cat<<version
+    script 0.1
+version
+}
 
 function operacion_recursiva()
 {
@@ -6,12 +32,83 @@ function operacion_recursiva()
     do
         if [ ! -d "${file}" ] ; then
             nombre=$(basename "$file")
-            echo "$nombre is a file"
+            encrypt "$encryption" "${file}"
+            echo "$nombre:$hash">>Hashes.txt
         else
-            echo "${file} es un directorio"
+            echo "$file">>Hashes.txt
             operacion_recursiva "${file}/"
         fi
     done
 }
-operacion_recursiva $1
 
+function encrypt()
+{
+    case $1 in
+        sha1)
+            hash=$(sha1sum "$2")
+            return 1
+        ;;
+        sha224)
+            hash=$(sha224sum "$2")
+            return 1
+        ;;
+        sha256)
+            hash=$(sha256sum "$2")
+            return 1
+        ;;
+        sha512)
+            hash=$(sha512sum "$2")
+            return 1
+        ;;
+        md5)
+            hash=$(md5sum "$2")
+            return 1
+        ;;
+    esac
+}
+
+while true; do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+        ;;
+        -V|--version)
+            version
+            exit 0
+        ;;
+        --sha1)
+            encryption=sha1
+            shift
+            break 2
+        ;;
+        --sha224)
+            encryption=sha224
+            shift
+            break 2
+        ;;
+        --sha256)
+            encryption=sha256
+            shift
+            break 2
+        ;;
+        --sha512)
+            encryption=sha512
+            shift
+            break 2
+        ;;
+        --md5)
+            encryption=md5
+            shift
+            break 2
+        ;;
+        --)
+            shift
+            break 2
+        ;;
+    esac
+    shift
+done
+
+echo "$1">>Hashes.txt
+operacion_recursiva $1
