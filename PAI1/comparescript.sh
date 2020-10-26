78d7c157b2c690176bc9823467ffebdb8dd1daf9
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-10-26 23:29:20 +0100

    comparescript.sh now includes a proper help message and allows addition of multiple options

--- PAI1/comparescript.sh
+++ PAI1/comparescript.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-if [ "$1" == "-h" ]; 
-then
-	echo "Usage: `basename $0` fichero1 fichero2";
-	exit 0;
-else
-	antiguo=$1;
-	nuevo=$2;
+usage="$(basename "$0") [-h] filename1 filename 2
+This script calculates the number of lines that differ between 2 
+files and write them in a txt file (output.txt)
 
-	diff -W999 --side-by-side $antiguo $nuevo | sed '/^[^\t]*\t\s*|\t\(.*\)/{s//\1 U/;b};/^\([^\t]*\)\t*\s*<$/{s//\1 D/;b};/^.*>\t\(.*\)/{s//\1 N/;b};d' > output.txt;
-fi
+where:
+    -h  show this help text"
+
+
+while getopts ':hs:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    **)
+		antiguo=$1;
+		nuevo=$2;
+		diff -W999 --side-by-side $antiguo $nuevo | sed '/^[^\t]*\t\s*|\t\(.*\)/{s//\1 U/;b};/^\([^\t]*\)\t*\s*<$/{s//\1 D/;b};/^.*>\t\(.*\)/{s//\1 N/;b};d' > output.txt;
+  esac
+done
+shift $((OPTIND - 1))
+
+antiguo=$1;
+nuevo=$2;
+
+diff -W999 --side-by-side $antiguo $nuevo | sed '/^[^\t]*\t\s*|\t\(.*\)/{s//\1 U/;b};/^\([^\t]*\)\t*\s*<$/{s//\1 D/;b};/^.*>\t\(.*\)/{s//\1 N/;b};d' > output.txt;
+
