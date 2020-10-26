91b663d1f96daae130e5998d36e526c047685a6f
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-10-26 21:33:18 +0100

    failed to add -help

--- PAI1/comparescript.sh
+++ PAI1/comparescript.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+if [ "$1" == "-h" ]; 
+then
+	echo "Usage: `$0` fichero1 fichero2";
+	exit 0;
+else
+	antiguo=$1;
+	nuevo=$2;
 
-antiguo=$1
-nuevo=$2
-
-diff -W999 --side-by-side $antiguo $nuevo | sed '/^[^\t]*\t\s*|\t\(.*\)/{s//\1 U/;b};/^\([^\t]*\)\t*\s*<$/{s//\1 D/;b};/^.*>\t\(.*\)/{s//\1 N/;b};d' > output.txt
-
+	diff -W999 --side-by-side $antiguo $nuevo | sed '/^[^\t]*\t\s*|\t\(.*\)/{s//\1 U/;b};/^\([^\t]*\)\t*\s*<$/{s//\1 D/;b};/^.*>\t\(.*\)/{s//\1 N/;b};d' > output.txt;
+fi
