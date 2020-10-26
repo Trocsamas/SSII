dc3279358481cdd65ea43f367f0023c2441be05a
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-10-26 16:15:24 +0100

    added basic script to compare which lines are different between two hids files

--- PAI1/comparescript.sh
+++ PAI1/comparescript.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 
+antiguo=$1
+nuevo=$2
+
+diff -W999 --side-by-side $antiguo $nuevo | sed '/^[^\t]*\t\s*|\t\(.*\)/{s//\1 U/;b};/^\([^\t]*\)\t*\s*<$/{s//\1 D/;b};/^.*>\t\(.*\)/{s//\1 N/;b};d' > output.txt
+
