bafdd5be8b147d591292fbfea9c10c49029b6e70
Trocsamas <jesala31@gmail.com>
2020-11-18 12:30:45 +0100

    Forma rudimentaria de comunicacion con el servidor a traves de openssl

--- PAI2/SSLSecureSocket.py
+++ PAI2/SSLSecureSocket.py
@@ -6,7 +6,7 @@ Created on Wed Nov 18 08:49:20 2020
 @author: trocsamas
 """
 import socket, ssl
-# import pprint
+import pprint
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain(certfile="/home/trocsamas/certificate.pem", keyfile="/home/trocsamas/key.pem")
@@ -15,12 +15,44 @@ bindsocket = socket.socket()
 bindsocket.bind(('127.0.0.1', 10023))
 bindsocket.listen(5)
 
+userDict = {"30256459M":"Hola_1234"}
+
 def deal_with_client(connstream):
+    
+    message = "Hola Cliente, envíe su usuario\n".encode('utf-8')
+    connstream.send(message)
+    
     data = connstream.recv(1024)
-    # empty data means the client is finished with us
-    while data:
-        print(data)
-        data = connstream.recv(1024)
+    usuario = str(data,'utf-8').replace("\n","")
+    print(usuario)
+        
+    message = ("Gracias "+ usuario +", envíe su contraseña\n").encode('utf-8')
+    connstream.send(message)
+    
+    data = connstream.recv(1024)
+    password= str(data,'utf-8').replace("\n","")
+    print(password)
+    
+    if userDict[usuario] == password:
+         while data:
+             message = ("Gracias "+ usuario +", ¿que transacción desea realizar?\n").encode('utf-8')
+             connstream.send(message)
+             data = connstream.recv(1024)
+             print(data)
+             message = ("¿Desea realizar otra transacción?\n").encode('utf-8')
+             connstream.send(message)
+             data = connstream.recv(1024)
+             print(data)
+             if str(data,'utf-8').replace("\n","") == "no":
+                 message = ("Cerrando conexión, muchas gracias\n").encode('utf-8')
+                 connstream.send(message)
+                 data = None
+             
+    else:
+        message = ("Usuario o Contraseña incorrecto, cerrando conexión\n").encode('utf-8')
+        connstream.send(message)
+
+        
 
 while True:
     print("Waiting for connections...")
