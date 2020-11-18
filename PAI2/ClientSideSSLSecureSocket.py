5377cceafe5574f98231eb159a21c698ded47a14
Trocsamas <jesala31@gmail.com>
2020-11-18 18:01:34 +0100

    ClientSide cutre

--- PAI2/ClientSideSSLSecureSocket.py
+++ PAI2/ClientSideSSLSecureSocket.py
@@ -5,15 +5,43 @@ Created on Wed Nov 18 10:22:22 2020
 
 @author: trocsamas
 """
-import socket
-import ssl
+import socket, ssl, pprint
+import tkinter
 
 
-hostname = 'www.python.org'
-# PROTOCOL_TLS_CLIENT requires valid cert chain and hostname
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.load_verify_locations('/home/trocsamas/certificate.pem')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 10023) as sock:
-    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-        print(ssock.version())
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(certfile="/home/trocsamas/certificate-client.pem", keyfile="/home/trocsamas/key-client.pem",password="PASSWORD2")
+conn = context.wrap_socket(socket.socket(socket.AF_INET),server_hostname="127.0.0.1")
+conn.connect(("127.0.0.1", 10023))
+
+
+
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+conn.send(b"30256459M")
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+conn.send(b"Hola_1234")
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+conn.send(b"12 12 1200")
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+conn.send(b"si")
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+conn.send(b"13 12 1200")
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+conn.send(b"no")
+data = conn.recv(1024)
+pprint.pprint(data.decode('utf-8'))
+
+
