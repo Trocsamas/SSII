b42fcbcce9a3c8358bee06841e678d18b40df58a
Trocsamas <jesala31@gmail.com>
2020-12-02 02:08:21 +0100

    Finalizacion de la CAI3

--- CAI3/Imagenes/Decrypter.sh
+++ CAI3/Imagenes/Decrypter.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 mkdir "$1"/"$2"/DECRYPT/
+key="$3"
+iv="$4"
 for file in "$1"/"$2"/*
 do
     if [ -f "${file}" ] ; then
         nombre=$(basename "$file" .bmp)
-        openssl enc $2 -d -in "${file}" -out "$1"/"$2"/DECRYPT/"$nombre"DECRYPT"$2".bmp  -K 1234567812345678123456781234567812345678123456781234567812345678 -iv 00000000000000000000000000000000
+        openssl enc $2 -d -in "${file}" -out "$1"/"$2"/DECRYPT/"$nombre"DECRYPT"$2".bmp  -K "$key" -iv "$iv"
         
     fi
 done
